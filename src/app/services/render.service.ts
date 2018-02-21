c45122a9ac263876f7dd93491e399423d69c25c0
l08084 <l08084@shibaura-it.ac.jp>
2018-02-21 19:00:22 +0900

    add drawBlock()

--- src/app/services/render.service.ts
+++ src/app/services/render.service.ts
@@ -25,43 +25,51 @@ export class RenderService {
    * @memberof AppComponent
    */
   render() {
-    if (this.context) {
-      this.context.clearRect(0, 0, MyConstant.BOARD_WIDTH, MyConstant.BOARD_HEIGHT); // 一度キャンバスを真っさらにする
-      this.context.strokeStyle = '#FFFF66'; // えんぴつの色を黒にする
+    this.context.clearRect(0, 0, MyConstant.BOARD_WIDTH, MyConstant.BOARD_HEIGHT); // 一度キャンバスを真っさらにする
+    this.context.strokeStyle = '#FFFF66'; // えんぴつの色を黒にする
 
-      Observable.combineLatest(
-        this.board$,
-        this.current$,
-        this.currentX$,
-        this.currentY$,
-        (board, current, currentX, currentY) => {
-          // 盤面を描画する
-          for (let x = 0; x < MyConstant.ROWS; x += 1) {
-            for (let y = 0; y < MyConstant.COLS; y += 1) {
-              if (board[y][x]) { // マスが空、つまり0ではなかったら
-                this.context.fillStyle = '#18A03C';
-                // マスを描画
-                this.drawBlock(x, y);
-              }
-            }
-          }
-
-          // 操作ブロックを描画する
-          for (let y = 0; y < 4; y += 1) {
-            for (let x = 0; x < 4; x += 1) {
-              if (current[y][x]) {
-                // マスの種類に合わせて塗りつぶす色を設定
-                this.context.fillStyle = '#18A03C';
-                // マスを描画
-                this.drawBlock(currentX + x, currentY + y);
-              }
+    Observable.combineLatest(
+      this.board$,
+      this.current$,
+      this.currentX$,
+      this.currentY$,
+      (board, current, currentX, currentY) => {
+        // 盤面を描画する
+        for (let x = 0; x < MyConstant.ROWS; x += 1) {
+          for (let y = 0; y < MyConstant.COLS; y += 1) {
+            if (board[y][x]) { // マスが空、つまり0ではなかったら
+              this.context.fillStyle = '#18A03C';
+              // マスを描画
+              this.drawBlock(x, y);
             }
           }
         }
-      );
 
+        // 操作ブロックを描画する
+        for (let y = 0; y < 4; y += 1) {
+          for (let x = 0; x < 4; x += 1) {
+            if (current[y][x]) {
+              // マスの種類に合わせて塗りつぶす色を設定
+              this.context.fillStyle = '#18A03C';
+              // マスを描画
+              this.drawBlock(currentX + x, currentY + y);
+            }
+          }
+        }
+      }
+    );
+  }
 
-    }
+  drawBlock = (x: number, y: number): void => {
+       this.context.fillRect(MyConstant.BLOCK_WIDTH * x,
+                            MyConstant.BLOCK_HEIGHT * y,
+                            MyConstant.BLOCK_WIDTH - 1,
+                            MyConstant.BLOCK_HEIGHT - 1);
+
+    this.context.strokeRect(MyConstant.BLOCK_WIDTH * x,
+                            MyConstant.BLOCK_HEIGHT * y,
+                            MyConstant.BLOCK_WIDTH - 1,
+                            MyConstant.BLOCK_HEIGHT - 1);
   }
 
 }
